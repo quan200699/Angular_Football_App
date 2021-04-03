82138cc577bc243c4364edb559778afd499e15f9
quan200699 <51079189+quan200699@users.noreply.github.com>
2021-04-03 17:18:57 +0700

    get all team by league

---
+++ src/app/service/team.service.ts
@@ -0,0 +1,19 @@
+import {Injectable} from '@angular/core';
+import {environment} from '../../environments/environment';
+import {Observable} from 'rxjs';
+import {HttpClient} from '@angular/common/http';
+
+const API_URL = `${environment.apiUrl}`;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TeamService {
+
+  constructor(private http: HttpClient) {
+  }
+
+  getAllTeamFromLeague(id: number): Observable<any> {
+    return this.http.get<any>(`${API_URL}/teams/league/${id}`);
+  }
+}
