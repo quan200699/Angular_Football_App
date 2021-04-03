b8dacde4cdef14782703cd8a195b9642cdc6a432
quan200699 <51079189+quan200699@users.noreply.github.com>
2021-04-03 18:20:17 +0700

    get all team with datatable done

---
+++ src/app/team/list-team/list-team.component.ts
@@ -0,0 +1,33 @@
+import {Component, OnInit} from '@angular/core';
+import {TeamService} from '../../service/team.service';
+import {ActivatedRoute, ParamMap} from '@angular/router';
+import {DataTableService} from '../../service/data-table.service';
+
+@Component({
+  selector: 'app-list-team',
+  templateUrl: './list-team.component.html',
+  styleUrls: ['./list-team.component.css']
+})
+export class ListTeamComponent implements OnInit {
+  listTeam: any = [];
+
+  constructor(private teamService: TeamService,
+              private activatedRoute: ActivatedRoute,
+              private dataTableService: DataTableService) {
+    this.activatedRoute.paramMap.subscribe((paramMap: ParamMap) => {
+      const id = +paramMap.get('id');
+      this.getAllTeamByLeagueId(id);
+    });
+  }
+
+  ngOnInit() {
+  }
+
+  getAllTeamByLeagueId(id) {
+    this.teamService.getAllTeamFromLeague(id).subscribe(data => {
+      this.listTeam = data.api.teams;
+      this.dataTableService.createDataTable('teams');
+    });
+  }
+
+}
